9a3371941c6e4628f243a218de122f788c80840e
imranulHoque <imrancoder.javascript@gmail.com>
2025-05-05 09:38:10 +0600

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
+
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
         hostname: "u7ln15t0v7.ufs.sh",
         protocol: "https",
-        pathname : "/**"
+        pathname: "/**",
       },
     ],
   },
